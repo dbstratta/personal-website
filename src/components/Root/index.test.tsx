a27b7e8cb5d87454a2ce8e27d50ed47304e16481
Diego Stratta <strattadb@gmail.com>
2018-07-13 01:42:59 -0300

    fix: fix eslint dependency bug

--- src/components/Root/index.test.tsx
+++ src/components/Root/index.test.tsx
@@ -1,12 +1,10 @@
+import { shallow } from 'enzyme';
 import React from 'react';
-import ReactDOM from 'react-dom';
+
 import Root from './component';
 
 describe('<Root />', () => {
   test('renders without crashing', () => {
-    const div = document.createElement('div');
-
-    ReactDOM.render(<Root />, div);
-    ReactDOM.unmountComponentAtNode(div);
+    shallow(<Root />);
   });
 });
--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,2 +1,7 @@
 import 'jest-enzyme';
 import 'jest-styled-components';
+
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({ adapter: new Adapter() });
