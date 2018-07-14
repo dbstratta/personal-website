7c70d1e945b66f38e325515a57fca5fff6bc7c0e
Diego Stratta <strattadb@gmail.com>
2018-07-14 00:36:43 -0300

    fix: fix tests

--- src/components/Root/component.test.tsx
+++ src/components/Root/component.test.tsx
@@ -1,10 +1,11 @@
 import { shallow } from 'enzyme';
 import React from 'react';
 
+import theme from '../../theme';
 import Root from './component';
 
 describe('<Root />', () => {
   test('renders without crashing', () => {
-    shallow(<Root />);
+    shallow(<Root theme={theme} />);
   });
 });
