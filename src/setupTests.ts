1fdc1f30e64208184361b6eff97050c51c63b4f1
Diego Stratta <strattadb@gmail.com>
2018-07-25 17:25:39 -0300

    refactor: improve naming

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,6 +1,6 @@
-import { configure } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import { configure as configureEnzyme } from 'enzyme';
+import EnzymeAdapter from 'enzyme-adapter-react-16';
 import 'jest-enzyme';
 import 'jest-styled-components';
 
-configure({ adapter: new Adapter() });
+configureEnzyme({ adapter: new EnzymeAdapter() });
