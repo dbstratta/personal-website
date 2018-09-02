ae10bc7df9af0873c49dbd8e0619739b9126724e
Diego Stratta <strattadb@gmail.com>
2018-09-02 08:54:18 -0300

    chore: remake yarn.lock

--- e2e/ciServer.js
+++ e2e/ciServer.js
@@ -1,9 +1,10 @@
-const http = require('http');
+const http = require('http2');
 
 const port = 3000;
 
 const ciServer = http.createServer((req, res) => {});
 
 ciServer.listen(port, () => {
+  // eslint-disable-next-line no-console
   console.log(`CI server is listening on port ${port}`);
 });
